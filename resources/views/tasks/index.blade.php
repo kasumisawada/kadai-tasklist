133ec21f11f05738854b4f69ebd8f94f429bbaeb
kasumi.sawada <kasumi.sawada@africascan.com>
2018-06-13 09:03:27 +0000

    bootstrap-tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,13 +5,26 @@
     <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
-        <ul>
-            @foreach ($tasks as $task)
-                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }} ({{ $task->status }})</li>
-            @endforeach
-        </ul>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Task</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->content }}</td>
+                        <td>{{ $task->status }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endif
     
-    {!! link_to_route('tasks.create', 'Post New Task') !!}
+    {!! link_to_route('tasks.create', 'Post New Task', null, ['class' => 'btn btn-primary'])  !!}
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,13 +4,25 @@
 
 <h1>id = {{ $task->id }} のtask詳細ページ</h1>
 
-    <p>タスク：{{ $task->content }}</p>
-    <p>ステータス：{{ $task->status }}</p>
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>Task</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+        <tr>
+            <th>Status</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+    </table>
     
-    {!! link_to_route('tasks.edit', 'Edit this task', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'Edit this task', ['id' => $task->id],['class' => 'btn btn-default']) !!}
     
      {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Delete') !!}
+        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
 @endsection
